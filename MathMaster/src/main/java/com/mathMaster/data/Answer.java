69aa212cbe4cf15c567a064dcf5bb8c4425be00d
Kevin Guan <Kevin Guan>
2017-03-17 12:12:01 -0400

    Pom jBCript configuration, added beans file

---
+++ MathMaster/src/main/java/com/mathMaster/data/Answer.java
@@ -0,0 +1,10 @@
+package com.mathMaster.data;
+
+public class Answer {
+	private int id;
+	private String username;
+	private String password;		// Encrpyt with JBCrypt
+	private String firstName;
+	private String lastName;
+	private UserRole userRoleId;
+}
---
+++ MathMaster/src/main/java/com/mathMaster/data/Type.java
@@ -0,0 +1,5 @@
+package com.mathMaster.data;
+
+public class Type {
+
+}
