f44406c31b09d021cd11ab0f2ea407ac3427eac7
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-20 19:41:17 -0400

    finished Course bean

--- MathMaster/src/main/java/com/mathMaster/model/Course.java
+++ MathMaster/src/main/java/com/mathMaster/model/Course.java
@@ -38,4 +38,63 @@ public class Course {
 	
 	@ManyToMany(mappedBy = "courses")
 	private List<Student> students = new ArrayList<Student>();
+
+	
+	public Course () {}
+	
+	public Course(int courseId, Teacher teacher, String courseName, String subjectName, List<Student> students) {
+		super();
+		this.courseId = courseId;
+		this.teacher = teacher;
+		this.courseName = courseName;
+		this.subjectName = subjectName;
+		this.students = students;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [courseId=" + courseId + ", teacher=" + teacher + ", courseName=" + courseName + ", subjectName="
+				+ subjectName + ", students=" + students + "]";
+	}
+
 }
