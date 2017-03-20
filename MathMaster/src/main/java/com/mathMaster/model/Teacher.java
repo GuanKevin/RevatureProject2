41f48a29974142e806992075b4f991084e2aa95b
Kevin Guan <Kevin Guan>
2017-03-20 17:09:12 -0400

    Removed old beans, added Teacher, Student Beans with hibernate code

---
+++ MathMaster/src/main/java/com/mathMaster/model/Teacher.java
@@ -0,0 +1,108 @@
+package com.mathMaster.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedQueries;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="M2_TEACHER")
+@NamedQueries({
+	
+})
+@NamedNativeQueries({
+	
+})
+public class Teacher {
+	@Id
+	@Column(name="TEACHER_ID")
+	@SequenceGenerator(name="TEACHER", sequenceName="TEACHER_PK_SEQ", initialValue=1, allocationSize=1)
+	@GeneratedValue(generator="TEACHER", strategy=GenerationType.SEQUENCE)
+	private int teacherId;
+	
+	@Column(name="USER_NAME")
+	private String userName;
+	
+	@Column(name="PASSWORD")
+	private String password;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	public Teacher() {}
+
+	public Teacher(int teacherId, String userName, String password, String firstName, String lastName, String email) {
+		super();
+		this.teacherId = teacherId;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [teacherId=" + teacherId + ", userName=" + userName + ", password=" + password + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+}
