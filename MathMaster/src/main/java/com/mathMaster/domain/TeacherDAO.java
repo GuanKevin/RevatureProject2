06949d933de44236877fa83289152c8043a74c60
Kevin Guan <Kevin Guan>
2017-03-21 13:34:32 -0400

    Small change to InsertStudent for test

--- MathMaster/src/main/java/com/mathMaster/domain/TeacherDAO.java
+++ MathMaster/src/main/java/com/mathMaster/domain/TeacherDAO.java
@@ -4,5 +4,5 @@ import com.mathMaster.model.Teacher;
 
 public interface TeacherDAO {
 	public Teacher getTeacherByUserName(String username);
-	public void createTeacher(Teacher teacher);
+	public boolean createTeacher(Teacher teacher);
 }
