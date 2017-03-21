d9c1399093cc7680ab80600d0625006dc9cdbc19
danliu277 <danliu277@gmail.com>
2017-03-21 13:10:51 -0400

    Changed arugments

--- MathMaster/src/main/java/com/mathMaster/domain/TestDAO.java
+++ MathMaster/src/main/java/com/mathMaster/domain/TestDAO.java
@@ -2,10 +2,11 @@ package com.mathMaster.domain;
 
 import java.util.List;
 
+import com.mathMaster.model.Course;
 import com.mathMaster.model.Test;
 
 public interface TestDAO {
-	public List<Test> getAllTest(int courseId);
+	public List<Test> getAllTest(Course course);
 	public Test getTestById(int testId);
 	public boolean createTest(Test test);
 }
