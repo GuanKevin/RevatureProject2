cfd5bdaabf8a06b7ae9fa4d817275a2866b3b421
danliu277 <danliu277@gmail.com>
2017-03-21 12:48:12 -0400

    Change arugment

--- MathMaster/src/main/java/com/mathMaster/domain/TakenTestDAO.java
+++ MathMaster/src/main/java/com/mathMaster/domain/TakenTestDAO.java
@@ -6,7 +6,7 @@ import com.mathMaster.model.Student;
 import com.mathMaster.model.TakenTest;
 
 public interface TakenTestDAO {
-	public List<TakenTest> getAllTakenTest(Student student);
+	public List<TakenTest> getAllTakenTest(int studentId);
 	public TakenTest getTakenTestById(int takenTestId);
 	public boolean takeTest(TakenTest takenTest);
 }
