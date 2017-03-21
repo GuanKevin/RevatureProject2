1b079bb0da6d8a872df4c1a9ccbf480bd9287f17
Kevin Guan <Kevin Guan>
2017-03-21 09:43:01 -0400

    Added Working InsertTeacher Test

--- MathMaster/src/main/java/com/mathMaster/domain/TeacherDAOImpl.java
+++ MathMaster/src/main/java/com/mathMaster/domain/TeacherDAOImpl.java
@@ -1,5 +1,34 @@
 package com.mathMaster.domain;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.mathMaster.model.Teacher;
+
 public class TeacherDAOImpl implements TeacherDAO{
 
+	/**
+	 * Queries the database for a teacher with 
+	 * their unique user-name and returns the user-name
+	 */
+	public Teacher getTeacherByUserName(String username) {
+		
+		return null;
+	}
+
+	public String getPassword(Teacher teacher) {
+		
+		return null;
+	}
+
+	public void createTeacher(Teacher teacher, Session session) {
+		Transaction tx = session.beginTransaction();
+		
+		try {
+			session.save(teacher);
+			tx.commit();
+		} catch (Exception ex) {
+			tx.rollback();
+		}
+	}
 }
