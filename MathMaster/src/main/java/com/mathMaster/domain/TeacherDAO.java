25276cd9ac534c130b3867c78d8e5e13930fd63e
Kevin Guan <Kevin Guan>
2017-03-20 21:35:42 -0400

    Added TeacherDAO and TeacherDAOImpl, added few possible methods for TeacherDAO to implement

---
+++ MathMaster/src/main/java/com/mathMaster/domain/TeacherDAO.java
@@ -0,0 +1,9 @@
+package com.mathMaster.domain;
+
+public interface TeacherDAO {
+	/*public void login(String username, String password);
+	public boolean authTeacher(String username, String password);
+	public boolean createTest();
+	public void viewAllStudents();
+	public void viewStudentByTestScore();*/
+}
---
+++ MathMaster/src/main/java/com/mathMaster/domain/TeacherDAOImpl.java
@@ -0,0 +1,5 @@
+package com.mathMaster.domain;
+
+public class TeacherDAOImpl implements TeacherDAO{
+
+}
