f865ac2b99a87a58ec368ca31967aa7e83e872e7
danliu277 <danliu277@gmail.com>
2017-03-21 11:27:46 -0400

    Added testDAO and takenTestDAO implementation

--- MathMaster/src/main/java/com/mathMaster/domain/TakenTestDAO.java
+++ MathMaster/src/main/java/com/mathMaster/domain/TakenTestDAO.java
@@ -1,5 +1,12 @@
 package com.mathMaster.domain;
 
-public interface TakenTestDAO {
+import java.util.List;
 
+import com.mathMaster.model.Student;
+import com.mathMaster.model.TakenTest;
+
+public interface TakenTestDAO {
+	public List<TakenTest> getAllTakenTest(Student student);
+	public TakenTest getTakenTestById(int takenTestId);
+	public boolean takeTest(TakenTest takenTest);
 }
