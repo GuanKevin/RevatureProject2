c237ac16d0475f1e862698794a25606dcc1a8d80
Pier Yos <khmaipapi@yahoo.com>
2017-03-20 20:37:57 -0400

    Edit Student and Course DAO

---
+++ MathMaster/src/main/java/com/mathMaster/domain/CourseDAO.java
@@ -0,0 +1,15 @@
+package com.mathMaster.domain;
+
+import java.util.List;
+
+import com.mathMaster.model.Course;
+import com.mathMaster.model.Student;
+import com.mathMaster.model.Teacher;
+
+public interface CourseDAO {
+	public boolean createCourse(Teacher teacher, String courseName, String subjectName);
+	public Course getCourseById(int id);
+	public List<Course> getAllCourseByTeacherId(Teacher teacher);
+	public List<Course> getAllCourseByStudentId(Student student);
+	
+}
--- MathMaster/src/main/java/com/mathMaster/domain/StudentDAO.java
+++ MathMaster/src/main/java/com/mathMaster/domain/StudentDAO.java
@@ -5,5 +5,4 @@ import com.mathMaster.model.Student;
 public interface StudentDAO {
 	public Student getStudentByUsername(String username);
 	//public boolean authenticateStudent(Student student, String password);
-	
 }
