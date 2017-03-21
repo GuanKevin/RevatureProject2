ebcfa01ff99a743cffe344dc7b20022b03970408
danliu277 <danliu277@gmail.com>
2017-03-21 17:14:16 -0400

    Delete list methods

--- MathMaster/src/test/java/com/mathMaster/test/GetAllExamQuestions.java
+++ MathMaster/src/test/java/com/mathMaster/test/GetAllExamQuestions.java
@@ -1,25 +1,31 @@
 package com.mathMaster.test;
 
+import java.util.Set;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 
 import com.mathMaster.domain.CourseDAOImpl;
 import com.mathMaster.domain.ExamDAOImpl;
-import com.mathMaster.model.Exam;
+import com.mathMaster.model.Course;
+import com.mathMaster.model.Question;
 import com.mathMaster.util.M2SessionFactory;
 
-public class CreateExam {
-	
+public class GetAllExamQuestions {
 	@Test
 	public void Test() {
 		SessionFactory sf = M2SessionFactory.getSessionFactory();
 		Session session = sf.openSession();
 		
 		CourseDAOImpl courseDao = new CourseDAOImpl(session);
-		Exam exam = new Exam("Exam #3", courseDao.getCourseById(2)); 
+		Course course = courseDao.getCourseById(2); 
 		
-		System.out.println(new ExamDAOImpl(session).createExam(exam));
+		Set<Question> list = new ExamDAOImpl(session).getExamById(4).getQuestions();
+		
+		for(Question q:list){
+			System.out.println(q);
+		}
 		
 		session.close();
 		sf.close();
