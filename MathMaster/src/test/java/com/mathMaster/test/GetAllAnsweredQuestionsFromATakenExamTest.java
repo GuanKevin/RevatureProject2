cac63e147774d5fe6de24c53a0a28efc69c7e40f
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-21 16:38:33 -0400

    starting my tests

---
+++ MathMaster/src/test/java/com/mathMaster/test/GetAllAnsweredQuestionsFromATakenExamTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.mathMaster.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Denise
+ *
+ */
+public class GetAllAnsweredQuestionsFromATakenExamTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
