8bdf957d6f015045e1d20ccf813ed3cb466ab616
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-21 17:00:34 -0400

    update

--- MathMaster/src/test/java/com/mathMaster/test/AddQuestionTest.java
+++ MathMaster/src/test/java/com/mathMaster/test/AddQuestionTest.java
@@ -2,13 +2,24 @@ package com.mathMaster.test;
 
 import static org.junit.Assert.*;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+
+import com.mathMaster.domain.ExamDAOImpl;
+import com.mathMaster.util.M2SessionFactory;
 
 public class AddQuestionTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		SessionFactory sf = M2SessionFactory.getSessionFactory();
+		Session session = sf.openSession();
+		
+		new ExamDAOImpl(session).getExamById(4);
+		
+		session.close();
+		sf.close();
 	}
 
 }
