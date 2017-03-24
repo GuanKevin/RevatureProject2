71286a3dbcfd883cf7c90287f0e58a2733f65740
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-24 19:02:19 -0400

    QuestionController and AnsweredQuestion controller

--- MathMaster/src/main/java/com/mathMaster/model/Question.java
+++ MathMaster/src/main/java/com/mathMaster/model/Question.java
@@ -15,11 +15,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-//import org.junit.Ignore;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 /**
  * 
