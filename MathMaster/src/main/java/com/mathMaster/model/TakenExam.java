95b58c4aab71da389999ae2de3d1f9be9238226d
Kevin Guan <Kevin Guan>
2017-03-24 09:30:07 -0400

    Test commit

--- MathMaster/src/main/java/com/mathMaster/model/TakenExam.java
+++ MathMaster/src/main/java/com/mathMaster/model/TakenExam.java
@@ -20,8 +20,6 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 @NamedQueries({
 	
