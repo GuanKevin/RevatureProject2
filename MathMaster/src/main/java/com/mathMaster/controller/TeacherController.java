f03b0158bccbaaf1da8573d0394f58954edcd43e
Kevin Guan <Kevin Guan>
2017-03-27 10:19:17 -0400

    Unchanged

--- MathMaster/src/main/java/com/mathMaster/controller/TeacherController.java
+++ MathMaster/src/main/java/com/mathMaster/controller/TeacherController.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +18,7 @@ import com.mathMaster.service.Delegate;
 import com.mathMaster.util.Facade;
 
 @Controller
+@RequestMapping(value = "Teacher")
 public class TeacherController {
 	private Delegate businessDelegate;
 	
@@ -27,13 +27,25 @@ public class TeacherController {
 		this.businessDelegate = businessDelegate;
 	}
 
-	@RequestMapping(value = "Teacher/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	/**
+	 * http://localhost:7001/MathMaster/Teacher/Code_Blooded_KG
+	 * 
+	 * @param username
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<Teacher> getTeacherByUsername(@PathVariable String username) throws Exception {
 		return new ResponseEntity<Teacher>(businessDelegate.getTeacherByUserName(username), HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "Teacher/Course/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method=RequestMethod.GET) 
+	public String teacherView () {
+		return "teacherHome.html";
+	}
+	
+	@RequestMapping(value = "Course/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<Set<Course>> getAllCourseByTeacherUsername(@PathVariable String username) throws Exception {
 		Facade facade = new Facade();
