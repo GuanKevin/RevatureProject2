e93ce4a5d2281cafce171bf436174dabb92c4140
Kevin Guan <Kevin Guan>
2017-03-27 08:41:56 -0400

    Started LoginController, putting LoginController on hold

---
+++ MathMaster/src/main/java/com/mathMaster/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.mathMaster.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("welcomePage")
+public class LoginController {
+	
+	@RequestMapping(value="Login", method=RequestMethod.GET) 
+	public String welomePage() {
+		return "login.html";
+	}
+}
