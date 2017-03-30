3a16ebb605340707e76d5fab6c18fb9fc0c041d9
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-29 23:15:56 -0400

    create answered questions

--- MathMaster/src/main/webapp/pages/denisejs/enterAnsweredQuestions.js
+++ MathMaster/src/main/webapp/pages/denisejs/enterAnsweredQuestions.js
@@ -15,13 +15,14 @@ $(document).ready(function() {
 		var timeTaken = 1490543129564;      // hard coded
 		var studentUsername = "Student001"; // hard coded
 		
-		var examId = $(this).attr('id');    // the exam that has been choosen
+		//var examId = $('li.exams').attr('id');    // the exam that has been choosen
+		var examId = 121;
 		var takenExamId;
 		
 		// create taken exam and return the id
 		
-		var takenExam = new takenExam(score, timeTaken);
-		$.ajax("http://localhost:7001/MathMaster/TakenExam/" + studentUsername + examId, {
+		var takenExam = new TakenExam(score, timeTaken);
+		$.ajax("http://localhost:7001/MathMaster/TakenExam/" + studentUsername + "/" + examId, {
 			method : "POST",
 			data : JSON.stringify(takenExam),
 			headers : {
@@ -45,7 +46,7 @@ $(document).ready(function() {
 					// going through each answered question and adding 
 					// the object to the list before sending it to the database
 					// with the corresponding question and taken exam id
-					$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/new/" + takenExamId + quesID, {
+					$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/new/" + takenExamId + "/" + quesID, {
 						method : "POST",
 						data : JSON.stringify(answeredQues),
 						headers : {
@@ -53,6 +54,14 @@ $(document).ready(function() {
 						},
 						success : function(response) {
 							console.log(response);
+							// this ajax call will enter the list of answered questions 
+							// in the controller to the database
+							$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/submit", {
+								method : "POST",
+								success : function() {
+									console.log("answered questions have been inserted to the DB");
+								}
+							})
 						},
 						error : function() {
 							console.log("error");
@@ -61,14 +70,14 @@ $(document).ready(function() {
 					
 				})// end of for each
 				
-				// this ajax call will enter the list of answered questions 
+/*				// this ajax call will enter the list of answered questions 
 				// in the controller to the database
 				$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/submit", {
 					method : "POST",
 					success : function() {
 						console.log("answered questions have been inserted to the DB");
 					}
-				})
+				})*/
 			
 			},
 			error : function() {
