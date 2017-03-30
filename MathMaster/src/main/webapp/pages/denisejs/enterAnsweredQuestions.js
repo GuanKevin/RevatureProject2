e1996454f71b91e4874dce6406e0eec72f536413
dmontesdeoca <denise.j.m11@gmail.com>
2017-03-30 16:35:42 -0400

    still debugging

--- MathMaster/src/main/webapp/pages/denisejs/enterAnsweredQuestions.js
+++ MathMaster/src/main/webapp/pages/denisejs/enterAnsweredQuestions.js
@@ -33,52 +33,6 @@ $(document).ready(function() {
 				console.log("response: " + response);
 				takenExamId = response;
 				console.log("Taken Exam Id: " + takenExamId);
-				
-				var questionID = $('.questions');
-				$.each(questionID,function(index, question){
-					// question ID
-					var quesID = $(question).data('id');	
-					// answer choosen
-					var radioValue = $("input[name='optionsRadios"+ quesID +"']:checked").val();
-				
-					var answeredQues = new AnsweredQuestionObj(radioValue);
-						
-					// going through each answered question and adding 
-					// the object to the list before sending it to the database
-					// with the corresponding question and taken exam id
-					$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/new/" + takenExamId + "/" + quesID, {
-						method : "POST",
-						data : JSON.stringify(answeredQues),
-						headers : {
-							"Content-Type" : "application/json"
-						},
-						success : function(response) {
-							console.log(response);
-/*							// this ajax call will enter the list of answered questions 
-							// in the controller to the database
-							$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/submit", {
-								method : "POST",
-								success : function() {
-									console.log("answered questions have been inserted to the DB");
-								}
-							})*/
-						},
-						error : function() {
-							console.log("error");
-						}
-					})// end of ajax
-					
-				})// end of for each
-				
-				// this ajax call will enter the list of answered questions 
-				// in the controller to the database
-				/*$.ajax("http://localhost:7001/MathMaster/TansweredQuestion/submit", {
-					method : "POST",
-					success : function() {
-						console.log("answered questions have been inserted to the DB");
-					}
-				})*/
-			
 			},
 			error : function() {
 				console.log("error");
